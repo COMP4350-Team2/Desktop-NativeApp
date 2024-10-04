133c19f9c0a38d040e5287d5a609cb293ca07e5d
MAA-Git <85462061+MAA-Git@users.noreply.github.com>
2024-10-03 23:02:01 -0500

    Add auth0 to logged in window

--- src/LoggedInWindow.xaml.cs
+++ src/LoggedInWindow.xaml.cs
@@ -1,13 +1,28 @@
 ﻿using System.Windows;
+using Auth0.OidcClient;
 
 namespace Desktop_Frontend
 {
     public partial class LoggedInWindow : Window
     {
-        public LoggedInWindow()
+        private Auth0Client auth0Client;
+
+        // Constructor accepting Auth0Client
+        public LoggedInWindow(Auth0Client client)
         {
             InitializeComponent();
+            auth0Client = client; // Assign the passed instance
+        }
+
+        private async void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Log the user out
+            await auth0Client.LogoutAsync();
+
+            // Close the logged-in window and open the main window
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
         }
     }
 }
-
