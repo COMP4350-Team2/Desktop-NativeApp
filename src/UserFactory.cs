d0bc66c8e2931cad3bdc4e80f7e5de31816bb46c
MAA-Git <85462061+MAA-Git@users.noreply.github.com>
2024-10-04 20:01:26 -0500

    Add a user factory class

---
+++ src/UserFactory.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace Desktop_Frontend
+{
+    internal static class UserFactory
+    {
+        public static IUser CreateUser()
+        {
+            Auth0Config config = new Auth0Config();
+
+            if (config.ConfigValid)
+            {
+                // Load the configuration from the .env file and return a real User instance
+                return new User(config);
+            }
+            else
+            {
+                // Return a mock user instance when .env is not present
+                return new UserMock();
+            }
+        }
+    }
+}
