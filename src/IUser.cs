1531b2c48a8c23ad0e38e9271be3074317ecf7bf
MAA-Git <85462061+MAA-Git@users.noreply.github.com>
2024-10-04 19:59:37 -0500

    Add User Interface

---
+++ src/IUser.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Desktop_Frontend
+{
+    public interface IUser
+    {
+        public Task Login();
+
+        public Task Logout();
+
+        public bool LoggedIn();
+
+        public string UserName();
+    }
+}
