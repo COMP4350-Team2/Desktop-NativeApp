3b707a08c7e95aacb0402e07dde26959fdaa35a4
MAA-Git <85462061+MAA-Git@users.noreply.github.com>
2024-10-06 14:17:00 -0500

    Add mock for backend - hardcoded list

---
+++ src/BackendMock.cs
@@ -0,0 +1,28 @@
+﻿namespace Desktop_Frontend
+{
+    public class BackendMock : IBackend
+    {
+        private IUser user;
+        private List<Ingredient> ingredients;
+
+        // Constructor initializes the list of ingredients
+        public BackendMock(IUser user)
+        {
+            ingredients = new List<Ingredient>
+            {
+                new Ingredient("Apple", "Fruit"),
+                new Ingredient("Milk", "Dairy"),
+                new Ingredient("Rice", "Grain"),
+                new Ingredient("Eggs", "Protein"),
+                new Ingredient("Bread", "Grain")
+            };
+            this.user = user;
+        }
+
+        // Implementation of GetAllIngredients using the initialized list
+        public async Task<List<Ingredient>> GetAllIngredients()
+        {
+            return ingredients;
+        }
+    }
+}
