3bf7be9745c67c52d2cb2613ccd232bba8a36dff
MAA-Git <85462061+MAA-Git@users.noreply.github.com>
2024-10-08 19:57:49 -0500

    Add Backend configuration manager

---
+++ src/BackendConfig.cs
@@ -0,0 +1,33 @@
+﻿using DotNetEnv;
+
+namespace Desktop_Frontend
+{
+    public class BackendConfig
+    {
+        public string BackendUrl;
+        public string Create_User_Endpoint;
+        public string All_Ing_Endpoint;
+        public bool ConfigValid;
+
+        public BackendConfig() 
+        {
+            LoadEnvVars();
+            ConfigValid = ValidateEnvVars();
+        }
+
+        private void LoadEnvVars()
+        {
+            Env.TraversePath().Load();
+            BackendUrl = Env.GetString("BACKEND_URL");
+            Create_User_Endpoint = Env.GetString("CREATE_USER");
+            All_Ing_Endpoint = Env.GetString("ALL_INGREDIENTS");
+        }
+
+        private bool ValidateEnvVars()
+        {
+            return !(string.IsNullOrEmpty(BackendUrl) || string.IsNullOrEmpty(Create_User_Endpoint)
+               || string.IsNullOrEmpty(All_Ing_Endpoint));
+        }
+
+    }
+}
