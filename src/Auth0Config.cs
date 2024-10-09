12cae945923e646b45c7112a7023d8831ef2662b
MAA-Git <85462061+MAA-Git@users.noreply.github.com>
2024-10-08 20:25:33 -0500

    Use dedicated AUTH0.env for Auth0Config

--- src/Auth0Config.cs
+++ src/Auth0Config.cs
@@ -9,7 +9,7 @@ namespace Desktop_Frontend
         public string CallbackUrl;
         public string ApiIdentifier;
         public bool ConfigValid;
-        public string Identifier;
+        public string Audience;
 
         public Auth0Config()
         {
@@ -21,19 +21,20 @@ namespace Desktop_Frontend
 
         private void LoadEnvVars()
         {
-            Env.TraversePath().Load();
+            Env.TraversePath().Load("./AUTH0.env");
 
             Domain = Env.GetString("AUTH0_DOMAIN");
             ClientId = Env.GetString("AUTH0_CLIENT_ID");
             CallbackUrl = Env.GetString("AUTH0_CALLBACK_URL");
             ApiIdentifier = Env.GetString("AUTH0_API_IDENTIFIER");
-            Identifier = "https://cupboard/api";
+            Audience = Env.GetString("AUTH0_AUDIENCE");
         }
 
         private bool ValidateEnvVars()
         {
             return !(string.IsNullOrEmpty(Domain) || string.IsNullOrEmpty(ClientId)
-               || string.IsNullOrEmpty(CallbackUrl) || string.IsNullOrEmpty(ApiIdentifier));
+               || string.IsNullOrEmpty(CallbackUrl) || string.IsNullOrEmpty(ApiIdentifier)
+               || string.IsNullOrEmpty(Audience));
        
         }
     }
