132deda28be8cedd0166ec4ca66bffe611f351db
MAA-Git <85462061+MAA-Git@users.noreply.github.com>
2024-10-08 22:09:09 -0500

    Rm IUser from Backend & add BackendConfig instead. Add CreateUser method

--- src/Backend/Backend.cs
+++ src/Backend/Backend.cs
@@ -1,14 +1,22 @@
 ﻿using Desktop_Frontend.DSOs;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace Desktop_Frontend.Backend
 {
     public class Backend : IBackend
     {
-        private IUser user;
+        private BackendConfig config;
 
-        public Backend(IUser user)
+        private HttpClient HttpClient;
+
+        public Backend() : this(new BackendConfig()) { }
+
+        public Backend(BackendConfig config)
         {
-            this.user = user;
+            this.config = config;
+
+            HttpClient = new HttpClient();
         }
 
         // Implementation of GetAllIngredients, returning an empty list for now
@@ -20,7 +28,46 @@ namespace Desktop_Frontend.Backend
 
         public async Task<bool> CreateUser(IUser user)
         {
-            return await Task.FromResult(true);
+            bool success = false;
+
+            //Create request
+            string url = config.BackendUrl + config.Create_User_Endpoint;
+            string accessToken = user.GetAccessToken();
+            var request = new HttpRequestMessage(HttpMethod.Post, url);;
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            //Send request and get response
+            try
+            {
+                HttpResponseMessage response = await HttpClient.SendAsync(request);
+                success = await ValidateHTTPResponse(response);
+
+            }
+            catch (Exception)
+            {
+               success = false;
+            }
+
+
+            return success;
+        }
+
+        private async Task<bool> ValidateHTTPResponse(HttpResponseMessage response)
+        {
+            bool valid = false;
+
+            if(response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                if (responseBody.Contains("Item created successfully") ||
+                    responseBody.Contains("Item already exists."))
+                {
+                    valid = true;
+                }
+            }
+
+            return valid;
         }
     }
 }
