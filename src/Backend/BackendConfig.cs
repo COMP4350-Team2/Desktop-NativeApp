7c5949c827660ee5e4cffe9a80ca0bcfbb1d5f92
MAA-Git <85462061+MAA-Git@users.noreply.github.com>
2024-10-19 15:01:00 -0500

    Add comment blocks to BackendConfig.cs

--- src/Backend/BackendConfig.cs
+++ src/Backend/BackendConfig.cs
@@ -4,19 +4,33 @@ using System.Runtime.CompilerServices;
 
 namespace Desktop_Frontend.Backend
 {
+    /// <summary>
+    /// This class is responsible for reading the backend environment variables
+    /// </summary>
     public class BackendConfig
     {
         public string? BackendUrl;
         public string? Create_User_Endpoint;
         public string? All_Ing_Endpoint;
+
+        /// <summary>
+        /// bool to check if the backend configuration is valid
+        /// </summary>
         public bool ConfigValid;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BackendConfig"/> class
+        /// and loads environment variables from the .env file.
+        /// </summary>
         public BackendConfig()
         {
             LoadEnvVars();
             ConfigValid = ValidateEnvVars();
         }
 
+        /// <summary>
+        /// Reads the .env file and sets the instance variables accordingly
+        /// </summary>
         private void LoadEnvVars()
         {
 
@@ -41,12 +55,24 @@ namespace Desktop_Frontend.Backend
 
         }
 
+        /// <summary>
+        /// Checks if .env file is read correctly and variables assigned.
+        /// </summary>
+        /// <returns>Returns true if .env file read properly, false otherwise.</returns>
         private bool ValidateEnvVars()
         {
             return !(string.IsNullOrEmpty(BackendUrl) || string.IsNullOrEmpty(Create_User_Endpoint)
                || string.IsNullOrEmpty(All_Ing_Endpoint));
         }
 
+        /// <summary>
+        /// Gets the directory of the source file where this method is called from.
+        /// </summary>
+        /// <param name="sourceFilePath">
+        /// The full path of the source file calling this method. 
+        /// This parameter is automatically provided by the compiler using the <see cref="CallerFilePathAttribute"/> and should not be explicitly passed.
+        /// </param>
+        /// <returns>The directory of the source file.</returns>
         private string? GetSourceFileDirectory([CallerFilePath] string sourceFilePath = "")
         {
             return Path.GetDirectoryName(sourceFilePath);
