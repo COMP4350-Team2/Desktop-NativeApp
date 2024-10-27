28fdc3ecbbb921cbff1b1ab2f2799a5663100e11
MAA-Git <85462061+MAA-Git@users.noreply.github.com>
2024-10-27 15:25:40 -0500

    Add amount and unit to Ingredient DSO

--- src/DSOs/Ingredient.cs
+++ src/DSOs/Ingredient.cs
@@ -7,6 +7,8 @@
     {
         private string name;
         private string ingType;
+        private float amount;
+        private string unit;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Ingredient"/> class
@@ -24,7 +26,23 @@
         {
             this.name = name;
             this.ingType = ingType;
+            amount = 0;
+            unit = string.Empty;
         }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Ingredient"/> class with specified details.
+        /// </summary>
+        /// <param name="name">The name of the ingredient.</param>
+        /// <param name="ingType">The type of the ingredient.</param>
+        /// <param name="amount">The amount of the ingredient.</param>
+        /// <param name="unit">The unit of measurement for the ingredient amount.</param>
+        public Ingredient(string name, string ingType, float amount, string unit) : this(name, ingType)
+        {
+            this.amount = amount;
+            this.unit = unit;
+        }
+
 
         /// <summary>
         /// Gets the name of the ingredient.
@@ -61,5 +79,30 @@
         {
             this.ingType = ingType;
         }
+
+        /// <summary>
+        /// Gets the amount of the ingredient.
+        /// </summary>
+        /// <returns>The amount of the ingredient as a float.</returns>
+        public float GetAmount() { return amount; }
+
+        /// <summary>
+        /// Sets the amount of the ingredient.
+        /// </summary>
+        /// <param name="amount">The amount to set for the ingredient.</param>
+        public void SetAmount(float amount) { this.amount = amount; }
+
+        /// <summary>
+        /// Gets the unit of the ingredient.
+        /// </summary>
+        /// <returns>The unit of the ingredient as a string.</returns>
+        public string GetUnit() { return unit; }
+
+
+        /// <summary>
+        /// Sets the unit of the ingredient.
+        /// </summary>
+        /// <param name="unit">The unit to set for the ingredient.</param>
+        public void SetUnit(string unit) { this.unit = unit; }
     }
 }
