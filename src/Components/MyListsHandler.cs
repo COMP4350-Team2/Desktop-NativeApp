dd50e08b4f091fb77672c53f1b471004252b92de
MAA-Git <85462061+MAA-Git@users.noreply.github.com>
2024-11-24 21:34:55 -0600

    Remove unused imports

--- src/Components/MyListsHandler.cs
+++ src/Components/MyListsHandler.cs
@@ -1,10 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Effects;
-using System.Windows.Media.Media3D;
 using Desktop_Frontend.Backend;
 using Desktop_Frontend.DSOs;
 
